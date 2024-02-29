277e3e840b476d9cde9665680e3ff31336e81d7a
Will Alread <will@kratoform.com>
2024-02-28 18:14:57 -0600

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import { LoginButton } from '@/components/auth/login-button'
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
-    <main>
-      <p>Next Auth Template</p>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          🔐 Auth
+        </h1>
+        <div>
+          <LoginButton asChild>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
     </main>
   )
 }
